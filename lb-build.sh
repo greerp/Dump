8e496bbfa45a7f4ce3358518a4a164fc6447a6f7
Paul Greer <paul.greer@redpixie.com>
2017-05-31 16:07:04 +0100

    Create lb-build.sh

---
+++ lb-build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo 'Hello World' > hw.txt
+
+# Post Deployment Bash Script for ALGO1 Builder Server
+#wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+#rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+# Install Jenkins and Java
+#yum -y install jenkins
+#yum -y install java
+
+# Configure jenkins to listen on; port 10665
+#sed -rie 's/(JENKINS_PORT=)[0-9]+$/\110665/gi' /etc/sysconfig/jenkins
+# The firewall is stopped to allow access to jenkisn from outside vnet
+#systemctl stop firewalld
+
+# Start Jenkins daemon
+#service jenkins start
